14b1e5baeb28a989dd37e1ad90e95d6b41896736
nirroz <nir.rozen@gmail.com>
2017-12-11 15:28:59 +0200

    Adding 2 new mutators

---
+++ Sentinel/Mutators/MethodRemoverMutator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Sentinel.Mutators
+{
+    class MethodRemoverMutator : BaseMutator
+    {
+        public override string MutationName => "MethodRemoverMutator";
+
+        public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            if (node.AttributeLists.ToFullString().ToLower().Contains("testmethod"))
+            {
+                return base.VisitMethodDeclaration(node);
+            }
+
+            if (node.Body == null)
+            {
+                return base.VisitMethodDeclaration(node);
+            }
+
+            if (!ShouldRun(node)) { return base.VisitMethodDeclaration(node); }
+
+            var returnTypeString = node.ReturnType.ToFullString().Trim().ToLower();
+
+            switch (returnTypeString)
+            {
+                case "void":
+                    return node.WithBody(SyntaxFactory.Block());
+                case "int":
+                    return node.WithBody(
+                        SyntaxFactory.Block(
+                            SyntaxFactory.ReturnStatement(
+                                SyntaxFactory.ParenthesizedExpression(
+                                    SyntaxFactory.LiteralExpression(
+                                        SyntaxKind.NumericLiteralExpression,
+                                        SyntaxFactory.Literal(-1))))));
+                case "bool":
+                    return node.WithBody(
+                        SyntaxFactory.Block(
+                            SyntaxFactory.ReturnStatement(
+                                SyntaxFactory.ParenthesizedExpression(
+                                    SyntaxFactory.LiteralExpression(SyntaxKind.FalseLiteralExpression)))));
+
+
+                default:
+                    return node.WithBody(
+                        SyntaxFactory.Block(
+                            SyntaxFactory.ReturnStatement(
+                                SyntaxFactory.ParenthesizedExpression(
+                                    SyntaxFactory.DefaultExpression(node.ReturnType)))));
+            }
+        }
+    }
+}
---
+++ Sentinel/Mutators/RemoveElseMutator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Sentinel.Mutators
+{
+    class RemoveElseMutator : BaseMutator
+    {
+        public override string MutationName => "RemoveElseMutator";
+
+        public override SyntaxNode VisitElseClause(ElseClauseSyntax node)
+        {
+            if (!ShouldRun(node)) { return base.VisitElseClause(node); }
+
+            var newNode = node.WithStatement(SyntaxFactory.EmptyStatement());
+            return newNode;
+        }
+    }
+}
